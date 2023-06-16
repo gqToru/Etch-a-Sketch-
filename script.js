de001465bc0e1a7924aef4ad65e2fcee2813cf0a
Hector Mata <hrmb26@gmail.com>
2023-06-16 09:19:04 -0300

    rendered the grid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
+const numberGrid = 32 * 32;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < numberGrid; i++) {
   const grid = document.createElement("div");
   grid.className = "gridColor";
   container.appendChild(grid);
