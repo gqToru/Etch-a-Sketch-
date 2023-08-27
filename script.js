715944173aaee826b615b5858db328ff7cf57c60
Hector Mata <hrmb26@gmail.com>
2023-08-27 09:50:54 -0300

    added button to create grid

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const gridContainer = document.getElementById("gridContainer");
+const createGridButton = document.getElementById("createGridButton");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridContainer.appendChild(gridItem);
+function createGrid() {
+  let sizeofGrid = prompt("Enter the size of the grid");
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  if (sizeofGrid < 16 || sizeofGrid > 100) {
+    alert("Size cannot exceed 100 tiles");
+    sizeofGrid = 0;
+    createGrid();
+  }
+  for (let i = 0; i < sizeofGrid; i++) {
+    for (let j = 0; j < sizeofGrid; j++) {
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      gridItem.addEventListener("mouseover", function () {
+        gridItem.classList.add("markedTile");
+      });
+      gridContainer.appendChild(gridItem);
+    }
   }
 }
+
+createGridButton.addEventListener("click", createGrid);
